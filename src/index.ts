3204fd5810190fa36150b50ba1aacf101e5bceb1
Valentin Lab <valentin.lab@kalysto.org>
2021-08-03 11:22:23 +0200

    first import
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+// From: https://github.com/prettier/prettier/issues/554#issuecomment-351517698
+// Many thanks @tqwhite
+
+import { format } from 'prettier'
+
+type filter = (s: string) => string
+
+let protectWS: filter = (s) => s.replace(/^[^\S\r\n]*$/gm, '/*linebreak*/')
+let unprotectWS: filter = (s) =>
+    s.replace(/^[^\S\r\n]*\/\*linebreak\*\/$/gm, '')
+
+
+export function run(argv: string[]) {
+    let inputData = ''
+    let pre: filter[] = []
+    let post: filter[] = []
+
+
+    let opt = {
+        singleQuote: true,
+        semi: false,
+        tabWidth: 4,
+    } as any
+    if (argv[0] === '--stdin-filepath') {
+        opt.filepath = argv[1]
+
+        if (opt.filepath.endsWith('.js') || opt.filepath.endsWith('.ts')) {
+            pre.push(protectWS)
+            post.push(unprotectWS)
+        }
+    }
+
+
+    // the rest
+
+    process.stdin.resume()
+    process.stdin.setEncoding('utf8')
+    process.stdin.on('data', function (data) {
+        inputData += data
+    })
+    process.stdin.on('end', () => {
+        let outString = inputData
+        pre.forEach((filter) => {
+            outString = filter(outString)
+        })
+        outString = format(outString, opt)
+        post.forEach((filter) => {
+            outString = filter(outString)
+        })
+        process.stdout.write(outString)
+    })
+}
